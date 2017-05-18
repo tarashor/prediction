0ce629dc43a85bc95ab465fd69bfbe34c3c439f4
taras <taras@frescano.se>
2017-05-18 19:43:59 +0200

    Fixing locale issues

--- src/main/java/com/tarashor/web/HomeController.java
+++ src/main/java/com/tarashor/web/HomeController.java
@@ -1,7 +1,17 @@
 package com.tarashor.web;
 
+import org.springframework.boot.autoconfigure.web.WebMvcProperties;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Locale;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -13,9 +23,21 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @RequestMapping(value="/")
 public class HomeController {
 
+    public static final String ACCEPT_LANGUAGE = "Accept-Language";
+
     @RequestMapping(method = GET)
     public String home(){
         return "home";
+    }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(HttpServletRequest request, Locale locale)
+    {
+        LocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+        Locale resolvedLocale = localeResolver.resolveLocale(request);
+        String language = request.getHeader(ACCEPT_LANGUAGE);
+        return ACCEPT_LANGUAGE + " = '" + language + "' locale = " + locale + " resolvedLocale = " + resolvedLocale;
     }
 
 }
